9f4c5e048b3d5b4e56301e73309762b2f587ee6d
le <le.xin@ximalaya.com>
2017-01-16 10:09:06 +0800

    播放页录音广告更改

--- src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -2,20 +2,22 @@ package com.example.myapplication;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        if(Danding.isOK) {
-            System.out.println("danding");
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add("淡定 --  " + i);
         }
-    }
 
-}
+        List<String> list2 = list.subList(0 , 5);
+        list.remove(2);
 
-
-class Danding {
-    public static boolean isOK = true;
-    static {
-        isOK = false;
+        for (String content : list2) {
+            System.out.println("nimei == " + content);
+        }
     }
 }
