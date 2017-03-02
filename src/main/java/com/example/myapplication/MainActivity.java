d9e8d30cd29b7d8695374ea7707c7934ccd3b74d
le <le.xin@ximalaya.com>
2017-03-02 15:38:38 +0800

    defualt

--- src/main/java/com/example/myapplication/MainActivity.java
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-import java.util.List;
-
 
 /**
  * Created by le.xin on 2017/1/9.
@@ -14,8 +12,6 @@ import java.util.List;
 public class MainActivity extends Activity {
 
     private RichWebView mRichWebView1;
-    private RichWebView mRichWebView2;
-    private RichWebView mRichWebView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,75 +19,271 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         mRichWebView1 = (RichWebView) findViewById(R.id.web_view1);
-        mRichWebView2 = (RichWebView) findViewById(R.id.web_view2);
-        mRichWebView3 = (RichWebView) findViewById(R.id.web_view3);
+//        mRichWebView1.setData("<p style=\"font-size:16px;line-height:28px;margin:20px 0px;" +
+//                "color:#333333\"><span style=\"font-size:16px;line-height:28px;color:#333333\"><span " +
+//                "style=\"font-size:16px;line-height:28px;color:#333333\"><strong data-flag=\"strong\" " +
+//                "style=\"font-size:16px;color:#FC5832;line-height:28px;" +
+//                "font-weight:normal\">（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）</strong></span></span></p><span " +
+//                "style=\"font-size:16px;line-height:28px;color:#333333\"></span><p style=\"font-size:16px;" +
+//                "line-height:28px;margin:20px 0px;color:#333333\"><span style=\"font-size:16px;line-height:28px;" +
+//                "color:#333333\">专辑内容：</span></p><p style=\"font-size:16px;line-height:28px;margin:20px 0px;" +
+//                "color:#333333\">2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：</p><p style=\"font-size:16px;line-height:28px;" +
+//                "margin:20px 0px;color:#333333\">年底总结汇报怎么做？</p><p style=\"font-size:16px;line-height:28px;margin:20px" +
+//                " 0px;color:#333333\">新的一年想涨涨工资，怎么向老板开口提？</p><p style=\"font-size:16px;line-height:28px;margin:20px " +
+//                "0px;color:#333333\">老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？</p><p style=\"font-size:16px;line-height:28px;" +
+//                "margin:20px 0px;color:#333333\">……</p><p style=\"font-size:16px;line-height:28px;margin:20px 0px;" +
+//                "color:#333333\">《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！</p><span style=\"font-size:16px;" +
+//                "line-height:28px;color:#333333\"></span><p style=\"font-size:16px;line-height:28px;margin:20px 0px;" +
+//                "color:#333333\"><span style=\"font-size:16px;line-height:28px;" +
+//                "color:#333333\">精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。</span></p>");
 
-
-
+        final String richStr = "<div class=\"rich_album_wrapper\">\n" +
+                "\t<div class=\"rich_album_tit-con\">\n" +
+                "\t\t<span class=\"rich_album_title\">\n" +
+                "\t\t\t简介\n" +
+                "\t\t</span>\n" +
+                "\t</div>\n" +
+                "</div>\n" +
+                "<p>\n" +
+                "\t<strong style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t\t（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）\n" +
+                "\t</strong>\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t专辑内容：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t年底总结汇报怎么做？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t新的一年想涨涨工资，怎么向老板开口提？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t……\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;line-height:28px;margin:20px 0px;color:#333333\">\n" +
+                "\t精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。\n" +
+                "</p>\n" +
+                "<div id=\"rich_album_bottom\">\n" +
+                "</div>\n" +
+                "<div id=\"rich_album_header\">\n" +
+                "</div>\n" +
+                "<div class=\"rich_album_wrapper\">\n" +
+                "\t<div class=\"rich_album_tit-con\">\n" +
+                "\t\t<span class=\"rich_album_title\">\n" +
+                "\t\t\t主讲人\n" +
+                "\t\t</span>\n" +
+                "\t</div>\n" +
+                "</div>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t专辑内容：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t年底总结汇报怎么做？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t新的一年想涨涨工资，怎么向老板开口提？\n" +
+                "</p>\n" +
+                "<p >\n" +
+                "\t<strong style=\"font-size:16px;color:#333333;\">\n" +
+                "\t\t老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？\n" +
+                "\t</strong>\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t……\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。\n" +
+                "</p>\n" +
+                "<div id=\"rich_album_bottom\">\n" +
+                "</div>\n" +
+                "<div id=\"rich_album_header\">\n" +
+                "</div>\n" +
+                "<div class=\"rich_album_wrapper\">\n" +
+                "\t<div class=\"rich_album_tit-con\">\n" +
+                "\t\t<span class=\"rich_album_title\">\n" +
+                "\t\t\t适合谁听\n" +
+                "\t\t</span>\n" +
+                "\t</div>\n" +
+                "</div>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t专辑内容：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t年底总结汇报怎么做？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t<strong>\n" +
+                "\t\t新的一年想涨涨工资，怎么向老板开口提？\n" +
+                "\t</strong>\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t……\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。\n" +
+                "</p>\n" +
+                "<div id=\"rich_album_bottom\">\n" +
+                "</div>\n" +
+                "<div id=\"rich_album_header\">\n" +
+                "</div>\n" +
+                "<div class=\"rich_album_wrapper\">\n" +
+                "\t<div class=\"rich_album_tit-con\">\n" +
+                "\t\t<span class=\"rich_album_title\">\n" +
+                "\t\t\t你将获得\n" +
+                "\t\t</span>\n" +
+                "\t</div>\n" +
+                "</div>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t专辑内容：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t年底总结汇报怎么做？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t新的一年想涨涨工资，怎么向老板开口提？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t……\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t<strong>\n" +
+                "\t\t《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！\n" +
+                "\t</strong>\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。\n" +
+                "</p>\n" +
+                "<div id=\"rich_album_bottom\">\n" +
+                "</div>\n" +
+                "<div id=\"rich_album_header\">\n" +
+                "</div>\n" +
+                "<div class=\"rich_album_wrapper\">\n" +
+                "\t<div class=\"rich_album_tit-con\">\n" +
+                "\t\t<span class=\"rich_album_title\">\n" +
+                "\t\t\t购买须知\n" +
+                "\t\t</span>\n" +
+                "\t</div>\n" +
+                "</div>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t专辑内容：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t年底总结汇报怎么做？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t新的一年想涨涨工资，怎么向老板开口提？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t……\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t<strong>\n" +
+                "\t\t精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。\n" +
+                "\t</strong>\n" +
+                "</p>\n" +
+                "<div id=\"rich_album_bottom\">\n" +
+                "</div>\n" +
+                "<div id=\"rich_album_header\">\n" +
+                "</div>\n" +
+                "<div class=\"rich_album_wrapper\">\n" +
+                "\t<div class=\"rich_album_tit-con\">\n" +
+                "\t\t<span class=\"rich_album_title\">\n" +
+                "\t\t\t其他说明\n" +
+                "\t\t</span>\n" +
+                "\t</div>\n" +
+                "</div>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t（本专辑五期音频均出自《好好说话》往期节目，切勿重复购买！）\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t专辑内容：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t2016年即将接近尾声，在职场打拼的你是否也需要面临以下情景：\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t<strong>\n" +
+                "\t\t年底总结汇报怎么做？\n" +
+                "\t</strong>\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t新的一年想涨涨工资，怎么向老板开口提？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t老板赶进度冲业绩，不合理的工作扔向你，该怎么拒绝？\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t……\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t《好好说话》精选专辑统统为你解决，让你在新的一年全面提升，升职加薪，走上人生巅峰！\n" +
+                "</p>\n" +
+                "<p style=\"font-size:16px;color:#333333;\">\n" +
+                "\t精选《好好说话》中关于职场的五期话题，对职场中常见的说话场景进行全方位解析，让你了解职场秘密，成功在激烈的职场竞争抢得先机。\n" +
+                "</p>\n" +
+                "<div id=\"rich_album_bottom\">\n" +
+                "</div>";
 
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mRichWebView1.setData("<img data-key=\"0\" src=\"http://fdfs.xmcdn" +
-                        ".com/group21/M07/6E/4E/wKgJKFh1zWPw85jPAAb3YCeRvkY628_mobile_large.jpg\" alt=\"\" " +
-                        "data-origin=\"http://fdfs.xmcdn.com/group21/M07/6E/4E/wKgJKFh1zWPw85jPAAb3YCeRvkY628.jpg\" " +
-                        "data-large=\"http://fdfs.xmcdn" +
-                        ".com/group21/M07/6E/4E/wKgJKFh1zWPw85jPAAb3YCeRvkY628_mobile_large.jpg\" " +
-                        "data-large-width=\"750\" data-large-height=\"5837\" data-preview=\"http://fdfs.xmcdn" +
-                        ".com/group21/M07/6E/4E/wKgJKFh1zWPw85jPAAb3YCeRvkY628_mobile_small.jpg\" " +
-                        "data-preview-width=\"140\" data-preview-height=\"1089\" " +
-                        "/><p><b>喜欢我的：</b></p><p><b>小姿专属会员适合每一个爱我的你，和未来会爱上我的你</b></p><br " +
-                        "/><p><b>恋爱中的：</b></p><p><b>情感生活出现各种迷茫困惑的</b><b>女生&男生</b></p><br " +
-                        "/><p><b>单身中的：</b></p><p><b>有真诚交友需求的单身女生&男生</b></p><br " +
-                        "/><strong>*（此专辑为小姿粉丝的专属VIP会员的特别福利专辑）</strong><br /><p><u>快来一起参与情感互动解密吧！</u></p>" +
-                        "<br /><img data-key=\"1\" " +
-                        "src=\"http://fdfs.xmcdn.com/group25/M02/6E/AD/wKgJNlh1zXbx2xVsAAfryNZNpgc667_mobile_large" +
-                        ".jpg\" alt=\"\" data-origin=\"http://fdfs.xmcdn" +
-                        ".com/group25/M02/6E/AD/wKgJNlh1zXbx2xVsAAfryNZNpgc667.jpg\" data-large=\"http://fdfs.xmcdn" +
-                        ".com/group25/M02/6E/AD/wKgJNlh1zXbx2xVsAAfryNZNpgc667_mobile_large.jpg\" " +
-                        "data-large-width=\"750\" data-large-height=\"5891\" data-preview=\"http://fdfs.xmcdn" +
-                        ".com/group25/M02/6E/AD/wKgJNlh1zXbx2xVsAAfryNZNpgc667_mobile_small.jpg\" " +
-                        "data-preview-width=\"140\" data-preview-height=\"1099\" />...<span style=\"display:none\" " +
-                        "data-preview=\"true\"></span>");
+                System.out.println(richStr);
             }
         });
 
-        mRichWebView1.setData("<p><b>喜欢我的：</b></p><p><b>小姿专属会员适合每一个爱我的你，和未来会爱上我的你</b></p><br " +
-                "/><p><b>恋爱中的：</b></p><p><b>情感生活出现各种迷茫困惑的</b><b>女生&男生</b></p><br " +
-                "/><p><b>单身中的：</b></p><p><b>有真诚交友需求的单身女生&男生</b></p><br " +
-                "/><strong>*（此专辑为小姿粉丝的专属VIP会员的特别福利专辑）</strong><br /><p><u>快来一起参与情感互动解密吧！</u></p>");
-        mRichWebView1.setOnImageClick(new RichWebView.IOnImageClick() {
-            @Override
-            public void onClick(List<String> imgs, int index) {
-                System.out.println("看看  ===  " + imgs);
-            }
-        });
-
-        mRichWebView2.setData("<p><b>喜欢我的：</b></p><p><b>小姿专属会员适合每一个爱我的你，和未来会爱上我的你</b></p><br " +
-                "/><p><b>恋爱中的：</b></p><p><b>情感生活出现各种迷茫困惑的</b><b>女生&男生</b></p><br " +
-                "/><p><b>单身中的：</b></p><p><b>有真诚交友需求的单身女生&男生</b></p><br " +
-                "/><strong>*（此专辑为小姿粉丝的专属VIP会员的特别福利专辑）</strong><br /><p><u>快来一起参与情感互动解密吧！</u></p>");
-        mRichWebView3.setData("<span>吃得了法餐进得了大排档、背得起Fendi也穿得起帆布鞋的</span>氧气美少女罗希教你如何用最合适的钱买到最合适的东西：） <br />");
-    }
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mRichWebView1.onPause();
-        mRichWebView2.onPause();
-        mRichWebView3.onPause();
-    }
-
-    /**
-     * Called when the fragment is no longer resumed. Pauses the WebView.
-     */
-    @Override
-    public void onResume() {
-        mRichWebView1.onResume();
-        mRichWebView2.onResume();
-        mRichWebView3.onResume();
-        super.onResume();
+        mRichWebView1.setData(richStr);
     }
 
 
--- src/main/java/com/example/myapplication/danding/TestActivity.java
+++ src/main/java/com/example/myapplication/danding/TestActivity.java
@@ -15,40 +15,6 @@ public class TestActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_lay);
-//
-//        final FlutterFlakeView view = (FlutterFlakeView) findViewById(R.id.flutter);
-//        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                view.pause();
-//            }
-//        });
-//
-//        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                view.setRepeatCount(1);
-//                view.setFlutterItems(BitmapFactory.decodeResource(getResources() , R.drawable.ic_launcher) ,20);
-//                view.start();
-//            }
-//        });
-//
-//        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                view.stop();
-//            }
-//        });
-//
-//        view.setRepeatCount(3);
-//        view.setFlutterItems(BitmapFactory.decodeResource(getResources() , R.drawable.ic_launcher) ,8);
-//        view.start();
-//        view.setItemClick(new FlutterFlakeView.FlutterItemClick() {
-//            @Override
-//            public boolean onClick(FlutterFlakeView.FlutterItem item) {
-//                Toast.makeText(TestActivity.this, "item被点击了"+item.hashCode(), Toast.LENGTH_SHORT).show();
-//                return false;
-//            }
-//        });
+
     }
 }
